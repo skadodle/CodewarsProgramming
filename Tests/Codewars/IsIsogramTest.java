67f1e9a1e643c70f5df0aeffdbaa6cc752c7f7b0
skadodle <Slavikkop@gmail.com>
2023-08-29 00:48:32 +0300

    Delete ex.printStackTrace() from catch{} in IsIsogramTest. Also delete testNull in BinaryMultipleOf3Test.

--- Tests/Codewars/IsIsogramTest.java
+++ Tests/Codewars/IsIsogramTest.java
@@ -2,7 +2,8 @@ package Codewars;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class IsIsogramTest {
 
@@ -11,7 +12,6 @@ public class IsIsogramTest {
         try {
             IsIsogram.check(null);
         } catch (Exception ex) {
-            ex.getStackTrace();
             System.out.println(ex.getMessage());
         }
     }
