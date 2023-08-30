445f2e8cf749bffaee1eda770168e5db05fa4b78
skadodle <Slavikkop@gmail.com>
2023-08-30 19:01:16 +0300

    Reformat stupid solution and use instanceof instead of "!e.equals(e.toString())".

--- src/Codewars/ListFiltering.java
+++ src/Codewars/ListFiltering.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class ListFiltering {
     public static List<Object> filterList(final List<Object> list) {
-        return list.stream().filter(e -> !e.equals(e.toString())).toList();
+        // need to use "instanceof"
+        return list.stream().filter(e -> e instanceof Integer).toList();
     }
 }
