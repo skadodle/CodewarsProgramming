155ffa58d3d70fdbe82d93c67c1c5708b7ae3efc
skadodle <slavikkop@gmail.com>
2025-06-13 02:22:32 +0300

    Update JUnit4 to JUnit5 TESTS

--- Tests/Codewars/ListFilteringTest.java
+++ Tests/Codewars/ListFilteringTest.java
@@ -1,6 +1,6 @@
 package Codewars;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
--- Tests/Codewars/SecretRecoveryStringTest.java
+++ Tests/Codewars/SecretRecoveryStringTest.java
@@ -1,6 +1,6 @@
 package Codewars;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
